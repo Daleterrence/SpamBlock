f1c12ac354289fffeed0487b34ab3aff1799bf7f
Dale <27327941+Daleterrence@users.noreply.github.com>
2025-09-20 21:29:12 +0100

    v1.0.0 - Auto-updating, settings file
    Auto-updating and settings to enable/disable portions of the addon have been added. See the readme for how each setting works.

--- README.md
+++ README.md
@@ -6,12 +6,34 @@ A Windower 4 addon to make yell chat usable. Originally "[FuckOff](https://githu
 
 
 
-#### To Do
-- Add settings file
-- Add commands to add strings and characters in game.
-- Finish this readme with how to use info.
-- Buy milk and bread.
+#### How to Use
 
-#### Known Issues
-- After an extended period of time in the same zone, SpamBlock will cease functioning.
-  - This is an issue with Windower itself, after about 6 hours in a zone, the packet handler falls over and dies, only fix I'm currently aware of is to reload the addon.
+The addon is designed to be set-and-forget. Filters are manually updated by me and currently designed for use on the Bahamut World to prevent the sea of spam we've been getting recently related to RMT and mercing.
+
+
+**This addon will auto-update as new versions are released, and the filters are updated.**
+
+The settings file allows you to enable or disable which modules of the addon are used;
+
+* rmt - Turns the RMT filtering for selected strings in yell and shout messages on or off. (Default: True)
+* books - Turns off spam caused by players using skill-up books, suppressing their chat entries. (Default: True)
+* autoupdate - Turns the auto-updating function of the addon on or off. If disabled, you will need to maintain your filters yourself. (Default: True)
+* blist - Turns the blacklist portion of the addon on or off, this blocks ALL chat from players added to this list, normally used as a fall-back against endlessly spamming characters. (Default: True)
+
+
+
+#### To Do
+
+* Add commands to turn settings on or off. They can be enabled or disabled in the settings.xml file the addon generates for now.
+* Add a custom blacklist to allow people to add their own names to be filtered.
+* Buy milk and bread.
+
+
+
+### Known Issues
+
+
+
+* After an extended period of time in the same zone, SpamBlock will cease functioning.
+
+  * This is an issue with Windower itself, after about 6 hours in a zone, the packet handler falls over and dies, only fix I'm currently aware of is to reload the addon.
--- SpamBlock.lua
+++ SpamBlock.lua
@@ -1,39 +1,88 @@
 _addon.name = 'SpamBlock'
-_addon.version = '0.01'
+_addon.version = '1.0.0'
 _addon.author = 'DTR, original code by Chiaia'
-_addon.commands = {'spamblock','sbl'} -- Unused currently.
+_addon.commands = {'sbl','spamblock'} -- To be used for upcoming commands.
 
---TODO: Add settings file and commands.
+--TODO: Add commands.
 
 require('luau')
 packets = require('packets')
+local config = require('config')
 
-local block_skillup = true -- Setting to true blocks item-use events from skill-up book items. 
-local blacklist = T{'Frededde','Comedie','Boamna','Thanatoss','Lowesquadone','Justchao','Jamiei','Bazzarcat','Aboschitt','Wooohoo','Aeoniczaca','Yagwick'} -- Blocks all messages from defined player-characters entered here, E.G. 'Spammerguy','Badguy', etc. 
+local default = { 
+    rmt = true,
+    books = true,
+    autoupdate = true,
+	blist = true,
+}
 
--- Blacklist Reasons:
--- Frededde - Racist, sexist, asshole.
--- Comedie, Boamna, Thanatoss, Lowesquadone, Justchao, Jamiei, Bazzarcat, Aboschitt, Wooohoo, Aeoniczaca, Yagwick - Mercing
+local settings = config.load(default)
+local send_command = windower.send_command
 
-local black_listed_words = T{string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'CP500p','2100p','ML0-20/15m','New2025','V0toV25', '3M/run', '3M/hour', 'Aeonic Weapon*.*Mind'} -- Be careful adding terms unless you want to accidentally neuter shout and yell chat.
--- {string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'1%-99','Job Points.*2100','Job Points.*500','Job Points.*4m','JP.*2100','JP.*500','Capacity Points.*2100','Capacity Points.*500','CPS*.*2100','CPS*.*500','ｆｆｘｉｓｈｏｐ','Jinpu 99999','Jinpu99999','This is IGXE','Clear Mind*.*15mins rdy start','Reisenjima*.*Helms*.*T4*.*Buy','Aeonic Weapon*.*3zone*.*Buy','Tumult Curator*.*Kill','Aeonic Weapon*.*Mind',.*Buy','Selling Aeonic','Empy Weapons Abyssea','50 50 75'} - First two are '☆' and '★' symbols. Currently unused terms, only grab the lot if you're on Asura. 
-local black_listed_skill_pages = T{'6147','6148','6149','6150','6151','6152','6153','6154','6155','6156','6157','6158','6159','6160','6161','6162','6163','6164','6165','6166','6167','6168','6169','6170','6171','6172','6173','6174','6175','6176','6177','6178','6179','6180','6181','6182','6183','6184','6185'} -- IDs to block item-use events for, add to this by finding item IDs in resources/items.
+--Auto-update
+windower.register_event('load', function()
+    if settings.autoupdate and not _addon.version:endswith('dev') then
+        local ltn12 = require('ltn12')
+        local https = require('ssl.https')
+        function get_txt(url, callback)
+            if type(callback) ~= 'function' then return end
+            coroutine.schedule(function()
+                local body = {}
+                response, code = https.request{ url = url, sink = ltn12.sink.table(body), }
+                body = table.concat(body)
+                callback(response, code, body)
+            end, 1)
+        end
+        local version_url = "https://raw.githubusercontent.com/Daleterrence/SpamBlock/refs/heads/main/SpamBlock.lua"
+        local version_pattern = "_addon.version *= *['\"](.-)['\"]"
+        local file_path = windower.addon_path .. "SpamBlock.lua"
+        get_txt(version_url, function(response, code, body)
+            if code == 200 then
+                version = body:match(version_pattern)
+                    if version and version ~= _addon.version then
+                    windower.add_to_chat(207, _addon.name .. ": new version found (%s -> %s), updating.":format(_addon.version, version))
+                    -- Open the file in write mode
+                    local file = io.open(file_path, "w")
+                    -- Check if the file was opened successfully
+                    if file then
+                        file:write(body)  -- Write the content to the file
+                        file:close()         -- Close the file
+                        windower.add_to_chat(207, _addon.name .. ": Update successful.")
+                        send_command('@wait 0.5;lua reload ' .. _addon.name)
+                        return
+                    else
+                        windower.add_to_chat(207, _addon.name .. "Failed to open file: " .. file_path)
+                    end
+                end
+            else
+                windower.add_to_chat(207, _addon.name .. ": " .. code .. " Failed to get file.")
+            end
+        end)
+    end
+end)
+
+local blacklist = T{'Frededde','Comedie','Boamna','Thanatoss','Lowesquadone','Justchao','Jamiei','Bazzarcat','Aboschitt','Wooohoo','Aeoniczaca','Yagwick','Criofan','Attkins','Yagwica'} -- Blocks all messages from defined player-characters entered here, E.G. 'Spammerguy','Badguy', etc. 
+
+local black_listed_words = T{string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'CP500p','2100p','ML0-20/15m','New2025','V0toV25', '3M/run', '3M/hour', 'Aeonic Weapon*.*Mind','2100/20M','T1T2T3T4'} -- Strings the addon will look for in yell and shout chat, and block appropriately.
+-- Example Strings:
+ -- string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'1%-99','Job Points.*2100','Job Points.*500','Job Points.*4m','JP.*2100','JP.*500','Capacity Points.*2100','Capacity Points.*500','CPS*.*2100','CPS*.*500','ｆｆｘｉｓｈｏｐ','Jinpu 99999','Jinpu99999','This is IGXE','Clear Mind*.*15mins rdy start','Reisenjima*.*Helms*.*T4*.*Buy','Aeonic Weapon*.*3zone*.*Buy','Tumult Curator*.*Kill','Aeonic Weapon*.*Mind',.*Buy','Selling Aeonic','Empy Weapons Abyssea','50 50 75' - First two are '☆' and '★' symbols.
+local black_listed_skill_pages = T{'6147','6148','6149','6150','6151','6152','6153','6154','6155','6156','6157','6158','6159','6160','6161','6162','6163','6164','6165','6166','6167','6168','6169','6170','6171','6172','6173','6174','6175','6176','6177','6178','6179','6180','6181','6182','6183','6184','6185'} -- IDs to block item-use chatlog events for, add to this by finding item IDs in resources/items.
 
 windower.register_event('incoming chunk', function(id,data)
     if id == 0x017 then -- Incoming chat packet.
         local chat = packets.parse('incoming', data)
         local cleaned = windower.convert_auto_trans(chat['Message']):lower()
 
-		if blacklist:contains(chat['Sender Name']) then -- Blocks any messages from defined player-characters above, in any chat mode.
+		if blacklist:contains(chat['Sender Name']) and settings.blist then -- Blocks any messages from defined player-characters above, in any chat mode.
 			return true
-		elseif (chat['Mode'] == 1 or chat['Mode'] == 26) then -- Blocks RMT spam in shout (1) and yell (26) chat modes.
+		elseif (chat['Mode'] == 1 or chat['Mode'] == 26) and settings.rmt then -- Blocks RMT spam in shout (1) and yell (26) chat modes.
 			for k,v in ipairs(black_listed_words) do
 				if cleaned:match(v:lower()) then
 					return true
 				end
 			end
         end
-    elseif id == 0x028 and block_skillup then -- Blocks skill-up book spam.
+    elseif id == 0x028 and settings.books then -- Blocks skill-up book spam.
         local data = packets.parse('incoming', data)
         if black_listed_skill_pages:contains(data['Target 1 Action 1 Param']) then
             return true
