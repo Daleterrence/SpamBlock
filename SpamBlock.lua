f9b5939b60b3f39ebb8cfdb64bd596133d4b8b5b
Dale <27327941+Daleterrence@users.noreply.github.com>
2025-09-24 21:16:33 +0100

    1.1.60 - Filters updated.

--- SpamBlock.lua
+++ SpamBlock.lua
@@ -1,5 +1,5 @@
 _addon.name = 'SpamBlock'
-_addon.version = '1.1.55'
+_addon.version = '1.1.60'
 _addon.author = 'DTR, original code by Chiaia'
 _addon.commands = {'sbl','spamblock'} -- To be used for upcoming commands.
 
@@ -61,9 +61,9 @@ windower.register_event('load', function()
     end
 end)
 
-local blacklist = T{'Comedie','Boamna','Thanatoss','Lowesquadone','Justchao','Jamiei','Bazzarcat','Aboschitt','Wooohoo','Aeoniczaca','Aeoniczzzcq','Yagwick','Criofan','Attkins','Yagwica','Senaki','Killera'} -- Blocks all messages from defined player-characters entered here, E.G. 'Spammerguy','Badguy', etc. 
+local blacklist = T{'Comedie','Boamna','Thanatoss','Lowesquadone','Justchao','Jamiei','Bazzarcat','Aboschitt','Wooohoo','Aeoniczaca','Aeoniczzzcq','Yagwick','Criofan','Attkins','Yagwica','Senaki','Killera','Killerfa'} -- Blocks all messages from defined player-characters entered here, E.G. 'Spammerguy','Badguy', etc. 
 
-local black_listed_words = T{string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'CP500p','2100p','ML0-20/15m','New2025','V0toV25', '3M/run', '3M/hour', 'Aeonic Weapon*.*Mind','2100/20M','T1T2T3T4','3 Area Clear Mind','boosted v25','T1-T2/V0-V25'} -- Strings the addon will look for in yell and shout chat, and block appropriately.
+local black_listed_words = T{string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'CP500p','2100p','ML0-20/15m','New2025','V0toV25', '3M/run', '3M/hour', 'Aeonic Weapon*.*Mind','2100/20M','T1T2T3T4','3 Area Clear Mind','boosted v25','OdysseyNM T1-T2/V0-V25 T3/V0-V25 T4/V0-V25'} -- Strings the addon will look for in yell and shout chat, and block appropriately.
 -- Example Strings:
  -- string.char(0x81,0x69),string.char(0x81,0x99),string.char(0x81,0x9A),'1%-99','Job Points.*2100','Job Points.*500','Job Points.*4m','JP.*2100','JP.*500','Capacity Points.*2100','Capacity Points.*500','CPS*.*2100','CPS*.*500','ｆｆｘｉｓｈｏｐ','Jinpu 99999','Jinpu99999','This is IGXE','Clear Mind*.*15mins rdy start','Reisenjima*.*Helms*.*T4*.*Buy','Aeonic Weapon*.*3zone*.*Buy','Tumult Curator*.*Kill','Aeonic Weapon*.*Mind',.*Buy','Selling Aeonic','Empy Weapons Abyssea','50 50 75' - First two are '☆' and '★' symbols.
 local black_listed_skill_pages = T{'6147','6148','6149','6150','6151','6152','6153','6154','6155','6156','6157','6158','6159','6160','6161','6162','6163','6164','6165','6166','6167','6168','6169','6170','6171','6172','6173','6174','6175','6176','6177','6178','6179','6180','6181','6182','6183','6184','6185'} -- IDs to block item-use chatlog events for, add to this by finding item IDs in resources/items.
